5587a8854eb4e408a200f59c77e9f791943311e4
Liam Masters <liam.masters3@gmail.com>
2021-04-23 23:18:34 -0400

    Add poem and prompt generator

--- src/main.js
+++ src/main.js
@@ -207,16 +207,65 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var vowels = "aeiou";
 
 function preload() {}
 
 function create() {
-    console.log(sentiment.analyze('.'));
-    console.log(adjectives);
-    console.log(nouns);
+    console.log(generatePoem());
+    console.log(generatePrompt());
 }
 
 function update() {}
+
+function generateAdjectives(num) {
+    let sentence = '';
+
+    let adjIdx;
+    let adj;
+    for(let i = 0; i < num; i++) {
+        adjIdx = Math.round(Math.random() * adjectives.length);
+        adj = adjectives[adjIdx];
+        sentence = sentence + adj + ' ';
+    }
+
+    if (vowels.includes(sentence[0])) {
+        sentence = 'n ' + sentence;
+    } else {
+        sentence = ' ' + sentence;   
+    }
+    return sentence;
+}
+
+function generatePoem() {
+    let sentence = "I am but a" + generateAdjectives(3);
+    
+    let nounIdx = Math.round(Math.random() * nouns.length);
+    let noun = nouns[nounIdx];
+
+    sentence = sentence + noun + " and you, princess are a" + generateAdjectives(3);
+    
+    nounIdx = Math.round(Math.random() * nouns.length);
+    noun = nouns[nounIdx];
+
+    sentence = sentence + noun + ". Together we will become a" + generateAdjectives(3);
+
+    nounIdx = Math.round(Math.random() * nouns.length);
+    noun = nouns[nounIdx];
+    sentence = sentence + noun + '.';
+
+    return sentence;
+}
+
+function generatePrompt() {
+    let sentence = "Good morrow friends. I am lonely here in my castle and thus I am look for a" + generateAdjectives(3);
+    
+    nounIdx = Math.round(Math.random() * nouns.length);
+    noun = nouns[nounIdx];
+    sentence = sentence + noun + '.';
+
+    return sentence;
+}
 },{"sentiment":8}],3:[function(require,module,exports){
 module.exports={
     "😂": 1,
